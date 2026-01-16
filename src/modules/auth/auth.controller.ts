b26e914c45786a63bd70c4bddf3a8e7b19b53166
Gabriel Dias Santiago <gabrieldiassantiago@hotmail.com>
2026-01-15 21:34:57 -0300

    feat: Update login method to use unified authService.login and adjust refresh token handling

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -14,16 +14,18 @@ export const auth = new Elysia({ prefix: "/auth" })
             exp: "7d" 
         })
     )
-
+    
         .post(
         "/login",
 
         async ({ jwt, body, set, request, server }) => { 
+
             try {
 
                 const userAgent = request.headers.get('user-agent') || 'unknown'; //talvez seja irrelevante, mas sei la
                 
                 const ip = server?.requestIP(request);
+                
                 const ipAddress = ip ? 
                     (ip.address === '::1' || ip.address === '127.0.0.1' ? 'localhost' : ip.address) 
                     : 'unknown';
@@ -34,7 +36,7 @@ export const auth = new Elysia({ prefix: "/auth" })
                     mobile: request.headers.get('sec-ch-ua-mobile') === '?1',
                 };
 
-                const result = await authService.loginByPhone(
+                const result = await authService.login(
                     body.phone, 
                     body.password, 
                     jwt,
@@ -43,7 +45,7 @@ export const auth = new Elysia({ prefix: "/auth" })
                 );
 
                 set.headers['set-cookie'] = [
-                    `refreshToken=${result.refreshToken}; HttpOnly; Secure; SameSite=Strict; Path=/auth/refresh; Max-Age=2592000`
+                    `refreshToken=${result.refreshToken}; HttpOnly; Secure; SameSite=Strict; Path=/auth; Max-Age=2592000`
                 ].join(', ');
 
                 return { 
@@ -62,19 +64,21 @@ export const auth = new Elysia({ prefix: "/auth" })
         }
     )
 
-  
-    .post(
-        "/admin/login",
-        async ({ jwt, body, set }) => { //falta refatorar '-'
-            try {
-                return await authService.loginAdmin(body.email, body.password, jwt);
-            } catch (e: any) {
-                set.status = 401;
-                return { error: e.message };
-            }
-        },
-        {
-            body: LoginAdminSchema,
-            response: { 200: AuthResponseSchema, 401: AuthErrorSchema }
+    .post("/refresh",
+    async ({ jwt, headers, set }) => {
+        try {
+        const refreshToken = headers['cookie']?.match(/refreshToken=([^;]+)/)?.[1];
+
+        if (!refreshToken) {
+            throw new Error("Refresh token não fornecido");
         }
-    );
+
+        const result = await authService.refreshAccessToken(refreshToken, jwt);
+
+        return result;
+
+        } catch (e: any) {
+            set.status = 401;
+            return { error: e.message };
+        }
+    })
