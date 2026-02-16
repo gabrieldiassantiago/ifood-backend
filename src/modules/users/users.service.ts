7e644ed1611534e6839f93f7e91343c393e64035
Gabriel Dias Santiago <gabrieldiassantiago@hotmail.com>
2026-02-16 19:02:44 -0300

    refactor: Comment out unused getAllUsers method and remove email check in createUser

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -7,23 +7,19 @@ export class UsersService {
     )
     {}
 
-    async getAllUsers(): Promise<UserResponse[]> {
+    /*async getAllUsers(): Promise<UserResponse[]> {
         const users = await this.usersRepository.findAll();
         return users;
     }
+    */
 
     async getUserById(id: string): Promise<UserResponse | null> {
         const user = await this.usersRepository.findById(id);
         return user;
     }
-    
+
     async createUser(data: CreateUserInput): Promise<UserResponse> {
       
-        const existingUserByEmail = data.email ? await this.usersRepository.findByEmail(data.email) : null;
-
-        if (existingUserByEmail) {
-            throw new Error("Email já em uso");
-        }
         
         const existingUserByPhone = await this.usersRepository.findByPhone(data.phone);
 
@@ -42,6 +38,7 @@ export class UsersService {
         password: hashedPassword,
         
        })
+       
        return newUser;
 
     }
